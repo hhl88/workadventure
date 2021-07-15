b04a45babdb9cb41c08b2983b8aa989c804d224c
le <le@silpion.de>
2021-07-15 02:39:54 +0200

    ...

--- contrib/docker/build_front.sh
+++ contrib/docker/build_front.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+echo "====== Clearing dist js ======"
 rm -rf ../../front/dist/js/*
+echo "====== Preparing container ======"
 docker-compose -f docker-compose.build.yaml build front
+echo "====== Building dist ======"
 docker-compose -f docker-compose.build.yaml up front
+echo "====== Closing container ======"
+docker-compose down
+echo "====== Adding to git ======"
 git add ../../front/dist
+echo "====== DONE ======"
